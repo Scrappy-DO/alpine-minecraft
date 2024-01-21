d0650951a158f97cc6493267a10a1647f1b906bf
scrappy <101096371+Scrappy-DO@users.noreply.github.com>
2024-01-21 11:59:05 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 STARTDIR=$(pwd)
 
-apk add python3 py3-pip python3-dev alpine-sdk openjdk17-jre git curl doas libc++-dev linux-headers xz openssl-dev
+apk add python3 py3-pip python3-dev alpine-sdk openjdk17-jre git curl doas libc++-dev linux-headers xz openssl-dev udev
 
 git clone https://github.com/guludo/venv-run.git
 python3 setup.py install
