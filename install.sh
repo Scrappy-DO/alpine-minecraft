71ea6fdbb659af74fea07fcda21751fae8d6c4f4
scrappy-DO <scrappy.deoverlever@gmail.com>
2024-01-25 16:40:16 +0100

    added docker

--- install.sh
+++ install.sh
@@ -6,11 +6,11 @@ then
   echo "usage: install.sh <os/docker>"
   
 elif [[ "$1" == "alpine" ]]
-  
+then  
   sh install-scripts/alpine.sh
 
 elif [[ "$1" == "docker" ]]
-  
+then
   cd install-scripts
   docker-compose up
 
